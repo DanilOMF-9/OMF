cce59862cc3a2d674937553e70b4a538d12558f0
Danil <danilomflebedev1138@gmail.com>
2025-02-19 14:53:39 +0500

    9 коммит

--- DB.py
+++ DB.py
@@ -4,14 +4,20 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-#вЫБИРАЕМ ИМНА И ВОЗВРАЩАЕМ ПОЛЬЗОВАТЕЛЕЙ СТАРШЕ 25 ЛЕТ
-cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
+#Получение средний возрост пользователя для каждого возроста
+cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
 results = cursor.fetchall()
 
 #Вывод
 for row in results:
     print(row)
 
+#Фильтрацич гкрппы по среднему возпросту больше 30
+cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
+filtered_results = cursor.fetchall()
+
+for row in filtered_results:
+    print(row)
 #Сохранение изменения и закрытием соединение
 
 connection.close()
