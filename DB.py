643ffc82f9805cb23fbd3e5cc323b840c300005b
Danil <danilomflebedev1138@gmail.com>
2025-02-19 15:19:54 +0500

    14 коммит

--- DB.py
+++ DB.py
@@ -4,11 +4,11 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-#Вычисление суммы возрасов пользователей
-cursor.execute("SELECT SUM(age) FROM Users")
-total_age = cursor.fetchone()[0]
+#Вычисление среднего возраста пользователей
+cursor.execute("SELECT AVG(age) FROM Users")
+average_age = cursor.fetchone()[0]
 
-print("Общее количество пользователей:", total_age)
+print("Средний ввозрост пользователей:", average_age)
 connection.close()
 
 
