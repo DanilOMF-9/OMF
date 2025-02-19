de07f6699b7d80c3f745bcc6c8f4c9b758951221
Danil <danilomflebedev1138@gmail.com>
2025-02-19 20:32:18 +0500

    '27'

--- DB.py
+++ DB.py
@@ -3,25 +3,6 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-#Создание таблици Users
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users(
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-''')
-
-#сОЗДАНИЕ ТРИГИРРА ДЛЯ ОБНОВЛЕНИЯ ВРЕМЕНИ СОЗДАНИЯ ПР ИВСТАВКЕ НОВОЙ ЗВПИСИС
-cursor.execute('''
-CREATE TRIGGER IF NOT EXISTS uptade_created_at
-AFTER INSERT ON Users
-BEGIN
-UPDATE Users SET created_at = CURRENT_TIMESTAMP WHERE
-id = NEW.id;
-END;
-''')
-
+cursor.execute('CREATE INDEX idx_username ON Users (username)')
 connection.commit()
 connection.close()
