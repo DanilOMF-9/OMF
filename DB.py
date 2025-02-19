5f8e402ab30cdf592e5d050ad7d6dbb42e9a8c09
Danil <danilomflebedev1138@gmail.com>
2025-02-19 15:23:39 +0500

    16 коммит

--- DB.py
+++ DB.py
@@ -4,11 +4,11 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-#Нахождения минимум возрата
-cursor.execute("SELECT MIN(age) FROM Users")
-min_age = cursor.fetchone()[0]
+#Нахождения максимума возрата
+cursor.execute("SELECT MAX(age) FROM Users")
+max_age = cursor.fetchone()[0]
 
-print("Минимум ввозрост пользователей:", min_age)
+print("Минимум ввозрост пользователей:", max_age)
 connection.close()
 
 
