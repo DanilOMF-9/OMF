f9b0b608c90c16f178362f722b58a49ea983bca5
Danil <danilomflebedev1138@gmail.com>
2025-02-19 14:28:16 +0500

    Шестой коммит

--- DB.py
+++ DB.py
@@ -5,7 +5,7 @@ connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
 #Добавлене нового пользователя
-cursor.execute('INSERT INTO Users(username, email, age) VALUES (?,?,?)',('newuser','newuser@example.com', 28))
+cursor.execute('UPDATE Users SET age = ? WHERE username = ?',(29, 'newuser'))
 
 #Сохранение изменения и закрытием соединение
 connection.commit()
