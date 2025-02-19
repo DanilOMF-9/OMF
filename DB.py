16e6aa8319a4a4fb92333662960228c339ebbbe6
Danil <danilomflebedev1138@gmail.com>
2025-02-19 14:09:01 +0500

    Четвертый коммит

--- DB.py
+++ DB.py
@@ -1,5 +1,21 @@
 import sqlite3
 
-#Создание подключения к базе
+#Установка соединенияч с базой 
 connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+
+#Создание таблиц
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER
+)
+''')
+
+#Сохранение изменения и закрытием соединение
+connection.commit()
 connection.close()
+
+
