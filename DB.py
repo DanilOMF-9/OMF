b7f2b4e5276d130fcfbbc32286454e90546690ba
Danil <danilomflebedev1138@gmail.com>
2025-02-19 15:13:50 +0500

    12 коммит

--- DB.py
+++ DB.py
@@ -4,24 +4,11 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-#Получение средний возрост пользователя для каждого возроста
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
-results = cursor.fetchall()
+#Подсчет общего числа потльзователей
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
 
-#Выбираем и сортируем пользователей по возрасту по убыванию
-cursor.execute('''
-SELECT username, age, AVG(age)
-FROM Users
-GROUP BY age
-HAVING AVG(age)>?
-ORDER BY age DESC
-''', (30,))
-results = cursor.fetchall()
-
-for row in results:
-    print(row)
-#Сохранение изменения и закрытием соединение
-
+print("Общее количество пользователей:", total_users)
 connection.close()
 
 
